2c80230827617d7020b615b9f9b241f332704b31
alexonweb <alexonweb@protonmail.com>
2022-06-18 04:15:50 +0300

    0.4.3 alpha

--- examples.php
+++ examples.php
@@ -1,9 +1,23 @@
 <?php
 
-require 'src/smartCounter.php';
+require 'src/SmartCounter.php';
 
 $counter = new FriendlyWeb\SmartCounter();
 
 $counter->count();
 
 ?>
+
+<h1>SmartCounter exapmles</h1>
+
+<p>Total views <?php echo $counter->views() ?> 
+of this page <?php echo $counter->views(true) ?>.
+
+<p>Total visits <?php echo $counter->visits() ?> 
+of this page <?php echo $counter->visits(true) ?>
+
+<h2>Current statistics data</h2>
+
+<code>
+<?php echo $counter->rawStats() ?>
+</code>
