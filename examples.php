401b426d0294f485bada0d61ad7cb97a5ab44a3e
alexonweb <alexonweb@protonmail.com>
2022-07-02 23:14:43 +0300

    0.4.6

--- examples.php
+++ examples.php
@@ -6,24 +6,29 @@ $counter = new FriendlyWeb\SmartCounter();
 
 $counter->count();
 
+require 'src/SmartCounterView.php';
+
+$counterview = new FriendlyWeb\SmartCounterView();
+
+
 ?>
 
 <h1>SmartCounter exapmles</h1>
 
 <h2>Total views and visits</h2>
 
-<p>Views: <?php echo $counter->views() ?>
+<p>Views: <?php echo $counterview->views() ?>
 
-<p>Visits: <?php echo $counter->visits() ?>
+<p>Visits: <?php echo $counterview->visits() ?>
 
 <h2>This page</h2>
 
-<p>Views: <?php echo $counter->views(true) ?>
+<p>Views: <?php echo $counterview->views(true) ?>
 
-<p>Visits: <?php echo $counter->visits(true) ?>
+<p>Visits: <?php echo $counterview->visits(true) ?>
 
 <h2>Current statistics data file</h2>
 
 <pre>
-<?php print_r($counter->rawStats()) ?>
+<?php print_r($counterview->rawStats()) ?>
 </pre>
