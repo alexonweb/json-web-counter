024b38e274211949ff0a894a4fcf41c6bcfea6f7
alexonweb <alexonweb@protonmail.com>
2022-07-03 23:32:31 +0300

    0.4.6

--- examples.php
+++ examples.php
@@ -1,34 +1,35 @@
 <?php
 
 require 'src/SmartCounter.php';
+require 'src/SmartCounterView.php';
 
 $counter = new FriendlyWeb\SmartCounter();
 
-$counter->count();
-
-require 'src/SmartCounterView.php';
+$counter->count(); // count this page
 
 $counterview = new FriendlyWeb\SmartCounterView();
 
-
 ?>
 
-<h1>SmartCounter exapmles</h1>
-
-<h2>Total views and visits</h2>
-
-<p>Views: <?php echo $counterview->views() ?>
-
-<p>Visits: <?php echo $counterview->visits() ?>
-
-<h2>This page</h2>
-
-<p>Views: <?php echo $counterview->views(true) ?>
-
-<p>Visits: <?php echo $counterview->visits(true) ?>
-
-<h2>Current statistics data file</h2>
-
-<pre>
-<?php print_r($counterview->rawStats()) ?>
-</pre>
+<table border="2" width="400">
+<caption>Stats</caption>
+    <thead>
+        <tr>
+            <th>Pages</th>
+            <th>Visits</th>
+            <th>Views</th>
+        </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>This page</td>
+        <td><?php echo $counterview->visits(true) ?></td>
+        <td><?php echo $counterview->views(true) ?></td>
+    </tr>
+    <tr>
+        <td>Total</td>
+        <td><?php echo $counterview->visits() ?></td>
+        <td><?php echo $counterview->views() ?></td>
+    </tr>
+    </tbody>
+</table>
